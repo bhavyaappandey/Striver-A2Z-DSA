b829b6c0442776f634caa1bbe6040eb138e845f4
bhavyaappandey <bhavyaappandey@gmail.com>
2026-04-05 16:12:49 +0530

    Create 4.7-check-palindrome-string-using-recursion.cpp

---
+++ Basics/4.7-check-palindrome-string-using-recursion.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+bool isPalindrome (string s, int l, int r) {
+  if (l >= r) 
+    return true;
+  if (s[l] != s[r]) 
+    return false;
+  return isPalindrome (s, l + 1, r - 1);
+}
+
+int main() {
+  string s = "madam";
+  if (isPalindrome(s, 0, s.size() - 1))
+    cout << "Palindrome";
+  else
+    cout << "Not Palindrome";
+  return 0;
+}
