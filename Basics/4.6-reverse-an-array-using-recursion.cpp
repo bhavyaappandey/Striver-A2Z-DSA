dfa3b414cc401de090816bd1338a8de060494d65
bhavyaappandey <bhavyaappandey@gmail.com>
2026-04-05 16:10:46 +0530

    Create 4.6-reverse-an-array-using-recursion.cpp

---
+++ Basics/4.6-reverse-an-array-using-recursion.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void reverse (int arr[], int l, int r) {
+  if (l >= r) 
+    return;
+  swap(arr[l], arr[r]);
+  reverse(arr, l + 1, r - 1);
+}
+
+int main() {
+  int arr[] = {1, 2, 3, 4, 5};
+  int n = 5;
+  reverse(arr, 0, n - 1);
+  for (int i = 0; i < n; i++)
+    cout << arr[i] << " ";
+  return 0;
+}
